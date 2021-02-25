b4d8acd0c5d83817913b5f5578ad55475c1089e1
h183502 <heine.fjeld@gmail.com>
2021-02-25 14:35:43 +0100

    Added mobX functionality, basicly removing the props from the different components, and collecting them in a i single file

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -24,8 +24,8 @@ const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
     del: <T> (url: string) => axios.delete<T>(url).then(responseBody),
 }
 
@@ -34,7 +34,7 @@ const Activities = {
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: Activity) => axios.post<void>('/activities', activity),
     uptdate: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete<void>  (`/activites/${id}`)
+    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
  }
 
 const agent = {
