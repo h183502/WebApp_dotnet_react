a5a39bdad9699f74b50fc07201e9ac3678ef4919
h183502 <heine.fjeld@gmail.com>
2021-03-09 13:02:06 +0100

    Added entity relationships between activities and user attendance

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -7,6 +7,8 @@ using Persistence;
 using AutoMapper;
 using Application.Core;
 using Application.Activities;
+using Application.Interfaces;
+using Infrastructure.Security;
 
 namespace API.Extensions
 {
@@ -33,6 +35,8 @@ namespace API.Extensions
             services.AddMediatR(typeof(List.Handler).Assembly);
 
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            services.AddScoped<IUserAccessor, UserAccessor>();
 
             return services;
         }
