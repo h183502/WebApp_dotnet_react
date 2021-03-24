477a8f29ad1817e71d05b7791183a03d12cdfd35
h183502 <heine.fjeld@gmail.com>
2021-03-24 14:33:39 +0100

    Added pagination and functionallity to the event pane in the users profile. Also added filtering functionallity to the activities page

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -17,6 +17,11 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(command));
         }
+        [HttpGet("{username}/activities")]
+        public async Task<IActionResult> GetUserActivity(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new ListActivities.Query{Username = username, Predicate = predicate}));
+        }
         
     }
 }
